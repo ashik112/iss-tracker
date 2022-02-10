5d1cb12c58c8dd97745bc562eae29d56b4167ae8
Ashekur Rahman <md.ashekur@gmail.com>
2022-02-10 23:26:46 +0600

    refactor

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const path = require('path');
 const cors = require('cors');
 const request = require('request');
